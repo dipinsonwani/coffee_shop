44cbcc5b633ccc58f8ff9835e369ef393adcd5e3
dipinsonwani <dipinsonwani@gmail.com>
2021-04-04 16:22:56 +0530

    Review UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'Coffee Shop',
-        theme: ThemeData(
+        theme: ThemeData(backgroundColor: Colors.brown[100],
             primarySwatch: Colors.brown,
             textTheme: TextTheme(
-              subtitle1: TextStyle(
-                fontFamily: 'varela',
-                fontSize: 22.0,
-                fontWeight: FontWeight.w600,
-                color: Color(0xFF473D3A),
-              ),
-              subtitle2: TextStyle(
-                fontFamily: 'varela',
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF473D3A),
-              ),
-            )),
+                subtitle1: TextStyle(
+                  fontFamily: 'varela',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF473D3A),
+                ),
+                subtitle2: TextStyle(
+                  fontFamily: 'varela',
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF473D3A),
+                ),
+                headline1: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.brown[700]))),
         home: HomePage());
   }
 }
