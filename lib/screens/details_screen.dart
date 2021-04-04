1e6c87fc632043ebb57cc7f896b9f1f1eed40428
dipinsonwani <dipinsonwani@gmail.com>
2021-04-04 16:18:17 +0530

    Build UI of DetailsScreen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,31 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
+  final String description;
+  final String img;
+  final String coffeeName;
+  final double price;
+
+  const DetailsScreen(
+      {Key key, this.img, this.coffeeName, this.price, this.description})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       backgroundColor: Colors.brown[50],
       body: Stack(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height - 20,
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xFFF3B2B7),
+          Positioned(
+            bottom: 60,
+            child: Container(
+              height: MediaQuery.of(context).size.height - 20,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                img,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
           Positioned(
             top: MediaQuery.of(context).size.height / 2,
@@ -19,6 +35,50 @@ class DetailsScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(45),
                 color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(coffeeName,
+                            style: Theme.of(context).textTheme.headline1),
+                        Text(
+                          '₹$price',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text('Description :',style: TextStyle(),textAlign: TextAlign.left,),
+                    SizedBox(height: 20,),
+                    Text(
+                      description,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[600]
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Container(
+                      width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.brown[800],
+                        ),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('Place Order',style: TextStyle(color: Colors.white),),
+                        ))
+                  ],
+                ),
               ),
             ),
           )
