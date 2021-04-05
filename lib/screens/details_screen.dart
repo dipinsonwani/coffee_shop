0d96b3cc817f6a1ac2c4aaf7eb8df7bcb57fa1bc
dipinsonwani <dipinsonwani@gmail.com>
2021-04-05 13:47:28 +0530

    Add Checkout Modal Sheet

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:coffee_shop/widgets/counter.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -13,7 +14,7 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.brown[50],
+      backgroundColor: Colors.brown[50],
       body: Stack(
         children: [
           Positioned(
@@ -27,6 +28,7 @@ class DetailsScreen extends StatelessWidget {
               ),
             ),
           ),
+          //IconButton(icon: Icon(Icons.arrow_back_sharp),onPressed: (){},),
           Positioned(
             top: MediaQuery.of(context).size.height / 2,
             child: Container(
@@ -34,7 +36,7 @@ class DetailsScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(45),
-                color: Colors.white,
+                color: Colors.brown[50],
               ),
               child: Padding(
                 padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
@@ -52,38 +54,172 @@ class DetailsScreen extends StatelessWidget {
                       ],
                     ),
                     SizedBox(
-                      height: 50,
+                      height: 30,
                     ),
-                    Text('Description :',style: TextStyle(),textAlign: TextAlign.left,),
-                    SizedBox(height: 20,),
+                    Divider(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Description :',
+                      style: TextStyle(
+                          color: Colors.brown[800],
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18),
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Text(
                       description,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey[600]
-                      ),
+                      style: TextStyle(fontSize: 15, color: Colors.grey[500]),
                       textAlign: TextAlign.center,
                     ),
                     SizedBox(
-                      height: 150,
+                      height: 60,
                     ),
                     Container(
-                      width: 300,
+                        width: 300,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(25),
                           color: Colors.brown[800],
                         ),
                         child: FlatButton(
-                          onPressed: () {},
-                          child: Text('Place Order',style: TextStyle(color: Colors.white),),
+                          onPressed: () {
+                            buildShowModalBottomSheet(context);
+                          },
+                          //_placeOrderSheet(context),
+                          child: Text(
+                            'Make Order',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ))
                   ],
                 ),
               ),
             ),
-          )
+          ),
+          Positioned(
+              top: 30,
+              child: IconButton(
+                icon: Icon(Icons.arrow_back_sharp),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )),
         ],
       ),
     );
   }
+
+  Future buildShowModalBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              builder: (context) => Container(
+                                      //decoration: BoxDecoration(borderRadius:BorderRadius.circular(30)),
+
+                                      child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, top: 30),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        Row(
+                                          children: <Widget>[
+                                            Text('Quantity',
+                                                style:
+                                                    TextStyle(fontSize: 20)),
+                                            SizedBox(
+                                              width: 110,
+                                            ),
+                                            Counter()
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text('Sugar Cubes',
+                                                style:
+                                                    TextStyle(fontSize: 20)),
+                                            SizedBox(
+                                              width: 75,
+                                            ),
+                                            Counter()
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text('Coffee Spoons',
+                                                style:
+                                                    TextStyle(fontSize: 20)),
+                                            SizedBox(
+                                              width: 60,
+                                            ),
+                                            Counter()
+                                          ],
+                                        ),
+                                        Divider(
+                                          height: 50,
+                                        ),
+                                        // SizedBox()
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              right: 50),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment
+                                                    .spaceBetween,
+                                            children: <Widget>[
+                                              Column(
+                                                children: <Widget>[
+                                                  Text('Total',
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .subtitle1),
+                                                  Text('₹499'),
+                                                ],
+                                              ),
+                                              Container(
+                                                width: 200,
+                                                decoration: BoxDecoration(
+                                                  color: Colors.brown[700],
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          20),
+                                                ),
+                                                child: FlatButton(
+                                                  onPressed: () {},
+                                                  child: Text(
+                                                    'Place Order',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )),
+                              context: context);
+  }
 }
+
+// Future<Widget> _placeOrderSheet(BuildContext context) {
+//   return showModalBottomSheet(
+//       builder: (context) => Container(
+//             height: 200,
+//             child: Text('Hi'),
+//           ),
+//       context: context);
+// }
